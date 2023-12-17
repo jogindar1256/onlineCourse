f86c3df75df8c6c8254fcfbc1ff280ce8a55fff2
singhjogindar12 <30556271-singhjogindar12@users.noreply.replit.com>
2023-12-17 12:02:05 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home |SkillVista</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <!-- Header Section -->
+  <header>
+    <h1>SkillVista</h1>
+    <nav>
+      <!-- Navigation links go here -->
+    </nav>
+  </header>
+
+  <!-- Main Content Section -->
+  <main>
+
+    <!-- Courses Section -->
+    <section class="courses">
+      <h2 class="p-8">Popular Courses</h2>
+      <div class="course-grid">
+        <!-- Course 1 -->
+        <div class="course">
+          <img
+            src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXJsfGVufDB8fDB8fHww"
+            alt="Course 1 Image">
+          <div class="course-content">
+            <h3>Course Title 1</h3>
+            <p>Course description goes here.</p>
+            <a href="#">Enroll Now</a>
+          </div>
+        </div>
+
+        <!-- Course 2 -->
+        <div class="course">
+          <img
+            src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXJsfGVufDB8fDB8fHww"
+            alt="Course 2 Image">
+          <div class="course-content">
+            <h3>Course Title 2</h3>
+            <p>Course description goes here.</p>
+            <a href="#">Enroll Now</a>
+          </div>
+        </div>
+        <!-- Course 3 -->
+        <div class="course">
+          <img
+            src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXJsfGVufDB8fDB8fHww"
+            alt="Course 2 Image">
+          <div class="course-content">
+            <h3>Course Title 2</h3>
+            <p>Course description goes here.</p>
+            <a href="#">Enroll Now</a>
+          </div>
+        </div>
+        <!-- Course 4 -->
+        <div class="course">
+          <img
+            src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXJsfGVufDB8fDB8fHww"
+            alt="Course 2 Image">
+          <div class="course-content">
+            <h3>Course Title 2</h3>
+            <p>Course description goes here.</p>
+            <a href="#">Enroll Now</a>
+          </div>
+        </div>
+
+        <!-- Add more courses as needed -->
+
+      </div>
+    </section>
+
+    <!-- Teacher Registration Section -->
+    <section class="teacher-registration">
+      <h2>Become a Teacher</h2>
+      <p>Share your knowledge! Register as a teacher and start creating courses.</p>
+      <a href="#">Register as a Teacher</a>
+    </section>
+
+    <!-- Curriculum Section -->
+    <section class="curriculum">
+      <div class="curriculum-content">
+        <h2>Curriculum</h2>
+        <p>Our Instructors have a wealth of experience in the industry.</p>
+        <p>Years of Experience: <span class="experience-years">10</span></p>
+      </div>
+    </section>
+
+    <!-- offer section -->
+    <section class="offer">
+      <div class="offer-content">
+        <h2>Special Offer!</h2>
+        <p>Subscribe to our newsletter and get exclusive access to premium content.</p>
+        <!-- Subscription Form -->
+        <form class="subscribe-form">
+          <input type="email" placeholder="Enter your email">
+          <button type="submit">Subscribe Now</button>
+        </form>
+      </div>
+    </section>
+
+    <!-- Legacy Section -->
+    <section class="legacy">
+      <div class="legacy-content">
+        <h2>Our Legacy</h2>
+        <p>Discover our rich history and the impact we've made in empowering learners worldwide.</p>
+      </div>
+    </section>
+  </main>
+
+  <!-- Footer Section -->
+  <footer>
+    <!-- Footer content goes here -->
+  </footer>
+
+</body>
+
+</html>
